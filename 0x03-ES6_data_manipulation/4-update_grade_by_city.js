9a8dd59f62cc785271c5021b923c62d26605d9ee
loxiasblack <iliaschokairi@gmail.com>
2024-07-02 00:15:46 +0100

    find and update

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(array, city, newGrades) {
+  return array
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
