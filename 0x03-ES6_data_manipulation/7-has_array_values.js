484ed0ec603cc5e7cbb0fbe785bfcadeed182b53
loxiasblack <iliaschokairi@gmail.com>
2024-07-02 13:28:56 +0100

    push

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,8 @@
 export default function hasValuesFromArray(set, array) {
-  const newelement = array.map((element) => set.has(element));
-  const checkresult = newelement.every((result) => result);
-  return checkresult;
+  if (set instanceof Set && array instanceof Array) {
+    const newelement = array.map((element) => set.has(element));
+    const checkresult = newelement.every((result) => result);
+    return checkresult;
+  }
+  return NaN;
 }
