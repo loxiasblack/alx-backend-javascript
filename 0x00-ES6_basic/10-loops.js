6ff76891e5c773ad158bdb52268c507b0d99ead8
loxiasblack <iliaschokairi@gmail.com>
2024-07-07 16:59:13 +0100

    using for of loop to get the exact output

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (let idx of array) {
+    idx = appendString + idx;
+    newArray.push(idx);
+  }
+  return newArray;
+}
