612e8af840c8137d60f70b6312e95d639e57fd08
loxiasblack <iliaschokairi@gmail.com>
2024-07-07 15:36:22 +0100

    return with computed name

---
+++ 0x00-ES6_basic/8-main.js
@@ -0,0 +1,4 @@
+import getBudgetForCurrentYear from './8-getBudgetCurrentYear.js';
+
+console.log(getBudgetForCurrentYear(2100, 5200, 1090));
+console.log(getBudgetForCurrentYear(2000, 6000, 6000));
