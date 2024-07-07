94549d55509025b1dea26c41eb87aa2dd842e1c1
loxiasblack <iliaschokairi@gmail.com>
2024-07-07 16:08:16 +0100

    es6 propreties method

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,11 +3,12 @@ import getBudgetObject from './7-getBudgetObject';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
+    ...budget,
     getIncomeInDollars() {
-      return `$${budget.income}`;
+      return `$${income}`;
     },
     getIncomeInEuros() {
-      return `${budget.income} euros`;
+      return `${income} euros`;
     },
   };
   return fullBudget;
