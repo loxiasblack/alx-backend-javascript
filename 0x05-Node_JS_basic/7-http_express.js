fe6a0b84615aef8e7a18d9572079d2068eb700a4
loxiasblack <iliaschokairi@gmail.com>
2024-08-20 23:46:51 +0100

    s

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = 1245;
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
     reject(new Error('Cannot load the database'));
