aff9d860c1b19366ed4043f8a4d7023b66cc4d8f
loxiasblack <iliaschokairi@gmail.com>
2024-09-17 18:22:12 +0100

    n

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
   process.exit();
 });
