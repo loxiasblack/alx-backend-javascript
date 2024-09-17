8bd902a8d72bfdc6d87bf5e59904e1fc4f9b5b45
loxiasblack <iliaschokairi@gmail.com>
2024-09-17 20:36:01 +0100

    create a server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,11 +4,9 @@ const port = 1245;
 const host = 'localhost';
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
 app.listen(port, host, () => {
-  console.log('...');
+  console.log('');
 });
